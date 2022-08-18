547b9259fb3915007b512d8308e1742b72a9e218
NirashaMadubhashini <nirashamadu0729@gmail.com>
2022-08-18 21:31:51 +0530

    update

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const customer = require('./routes/customer')
+const item = require('./routes/item')
+const user = require ('./routes/user')
+const order = require ('./routes/Order')
+const orderDetail = require ('./routes/OrderDetails')
+
+const app = express()
+const port = 4000
+
+app.use(express.json())
+app.use('/customer', customer)
+app.use('/items',  item)
+app.use('/users',user)
+app.use('/orders',order)
+app.use('/orderDetail',orderDetail)
+
+
+
+app.listen(port, () => {
+    console.log(`app starting on ${port}`);
+})
